11ef914916b5a01177be1009eaac475dd932cc1a
firedevelop <firedevelop.com@gmail.com>
2019-01-04 12:56:38 +0100

    .

---
+++ android/app/src/main/kotlin/com/example/id0000145flutterdartexample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.id0000145flutterdartexample
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
